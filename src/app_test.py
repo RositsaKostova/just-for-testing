49b888768957fa49e280c11fa1f10243c873318e
RositsaKostova <97035764+RositsaKostova@users.noreply.github.com>
2024-12-27 15:19:52 +0200

    Update app_test.py

--- src/app_test.py
+++ src/app_test.py
@@ -1,20 +1,20 @@
 import unittest
 from app import app
+import werkzeug
 
+# Добавяне на фиктивен атрибут '__version__', ако не съществува
+if not hasattr(werkzeug, '__version__'):
+    werkzeug.__version__ = "2.0.0"  # Можеш да зададеш фиктивна стойност
 
 class TestApp(unittest.TestCase):
-
     def setUp(self):
         self.client = app.test_client()
 
     def test_hello_world(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn(
-            b"Hello! I am a Flask application running",
-            response.data
-        )
+        self.assertIn("Hello! I am a Flask application", response.get_data(as_text=True))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
