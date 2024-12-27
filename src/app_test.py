f507221c2a915c18d40d5794460ae45cee259441
RositsaKostova <97035764+RositsaKostova@users.noreply.github.com>
2024-12-27 14:14:14 +0200

    Update app_test.py

--- src/app_test.py
+++ src/app_test.py
@@ -1,7 +1,10 @@
 import unittest
-
 from app import app
+import werkzeug
 
+# Добавяне на съвместимост с новите версии на Werkzeug
+if not hasattr(werkzeug, '__version__'):
+    werkzeug.__version__ = "Unknown"
 
 class TestApp(unittest.TestCase):
     def setUp(self):
@@ -10,6 +13,7 @@ class TestApp(unittest.TestCase):
     def test_hello_world(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+        self.assertIn("Hello! I am a Flask application", response.get_data(as_text=True))
 
 
 if __name__ == "__main__":
