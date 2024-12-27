5c9421087ab7c41e17f045582830ffcc34b5b945
RositsaKostova <97035764+RositsaKostova@users.noreply.github.com>
2024-12-27 14:58:54 +0200

    Update app_test.py

--- src/app_test.py
+++ src/app_test.py
@@ -1,16 +1,14 @@
 import unittest
-
 from app import app
-
 
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
     def test_hello_world(self):
-        response = self.client.get("/")
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Hello! I am a Flask application running", response.data)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
